2bad706e2b533db7550f8eec6105fc07a3eea6d1
logonrm <logonrm@fiap.com.br>
2018-03-09 09:37:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/configuracaopersonagem/PersonagemActivity.kt
@@ -0,0 +1,25 @@
+package com.example.logonrm.configuracaopersonagem
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_personagem.*
+
+class PersonagemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personagem)
+
+        btCriar.setOnClickListener{
+            Toast.makeText(
+                    this, "Usuário criado", Toast.LENGTH_SHORT)
+                    .show()
+            
+
+
+        }
+
+
+    }
+}
